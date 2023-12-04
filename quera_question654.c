0cdec486b78c4afdf2b148c1ac5d105e252810a2
erfansalman <estaheri101@gmail.com>
2023-12-04 17:36:17 +0330

    fixed an issue

--- quera_question654.c
+++ quera_question654.c
@@ -2,23 +2,23 @@
 
 int main() {
     long long int n;
-    printf("Enter n: ");
     scanf("%lld", &n);
-    int a;
-    int b;
-    int c;
+    long long int a;
+    long long int b;
+    long long int c;
     int counter = 0;
     for (a = 1; a < (n / 2); a++) {
         for (b = a; b < (n / 2); b++) {
             c = n - a - b;
             if (c * c == a * a + b * b) {
                 counter++;
-                printf("|a: %d, b: %d, c: %d|", a, b, c);
+                printf("%lld %lld %lld\n", a, b, c);
+                return 0;
             }
         }
     }
     if (counter == 0) {
-        printf("Impossible!");
+        printf("Impossible");
     }   
     return 0;
 }
